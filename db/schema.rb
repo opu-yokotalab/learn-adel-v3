2acef141bc8440cddac6490626142d30db504a24
learn <learn@1a976859-c991-4926-a71f-f36caf5f6619>
2008-07-08 07:12:29 +0000

    migrateファイルを修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/adel_v3/trunk@18 1a976859-c991-4926-a71f-f36caf5f6619

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 15) do
 
   create_table "action_logs", :force => true do |t|
     t.integer  "user_id"
@@ -25,18 +25,26 @@ ActiveRecord::Schema.define(:version => 2) do
     t.text   "module_src"
   end
 
-  add_index "ent_modules", ["module_name"], :name => "ent_modules_module_name_key", :unique => true
+  create_table "ent_questions", :force => true do |t|
+    t.string  "question_name"
+    t.integer "max_point"
+  end
 
   create_table "ent_seqs", :force => true do |t|
     t.text   "seq_src"
     t.string "seq_title"
   end
 
-  create_table "learns", :force => true do |t|
-    t.string   "name"
-    t.string   "contents"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "ent_tests", :force => true do |t|
+    t.string  "test_name"
+    t.integer "max_sum_point"
+  end
+
+  create_table "level_logs", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "ent_seq_id"
+    t.integer  "level"
+    t.datetime "created_on"
   end
 
   create_table "module_logs", :force => true do |t|
@@ -53,6 +61,37 @@ ActiveRecord::Schema.define(:version => 2) do
     t.string   "event_arg"
     t.datetime "created_on"
     t.integer  "dis_code"
+  end
+
+  create_table "question_logs", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "ent_seq_id"
+    t.integer  "ent_module_id"
+    t.integer  "ent_test_id"
+    t.integer  "ent_question_id"
+    t.integer  "point"
+    t.datetime "created_on"
+  end
+
+  create_table "rule_search_time_logs", :force => true do |t|
+    t.integer "user_id"
+    t.string  "time_name"
+    t.time    "time_value"
+  end
+
+  create_table "seq_logs", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "ent_seq_id"
+    t.datetime "created_on"
+  end
+
+  create_table "test_logs", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "ent_seq_id"
+    t.integer  "ent_module_id"
+    t.integer  "ent_test_id"
+    t.integer  "sum_point"
+    t.datetime "created_on"
   end
 
   create_table "users", :force => true do |t|
